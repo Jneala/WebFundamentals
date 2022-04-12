e93713786f51ca99b0db4bd3c1416064db4a11ab
Jonathon Alsum <jneala@gmail.com>
2022-04-12 11:39:19 -0500

    playing with objects and the DOM, in coding dojo

---
+++ Objects_and_DOM/playingWithObjects.js
@@ -0,0 +1,16 @@
+var users = [
+  {
+      name: "Michael",
+      age: 37
+  },
+  {
+      name: "John",
+      age: 30
+  },
+  {
+      name: "David",
+      age: 27
+  }
+];
+
+console.log(users[1].age);
